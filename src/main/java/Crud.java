6d52c62ba193538e8444a63aae78eb99090541ab
volgofant <volgofant@mail.ru>
2019-10-30 15:15:08 +0300

    repair gitignore

--- src/main/java/Crud.java
+++ src/main/java/Crud.java
@@ -2,7 +2,6 @@ import java.sql.SQLException;
 
 public class Crud {
     public static void main(String[] args) throws SQLException {
-        new InsertDate().insertByDefaultId("Виктор", "Москва", 28);
         new SelectDate().selectAllDate();
     }
 }
